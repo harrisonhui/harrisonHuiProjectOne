b59060232b3b78f8b7155d73a5c78da86577bc24
Harrison <harrison.hui.524@gmail.com>
2022-03-29 15:37:38 -0400

    added js functionality to add new comments

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const formElement = document.querySelector('form');
+
+formElement.addEventListener('submit', function(event) {
+    console.log("test")
+    event.preventDefault();
+    const commentInput = document.querySelector('textarea');
+    const commentNew = commentInput.value
+
+    if (commentNew) {
+    const listItem = document.createElement('li')
+    listItem.innerHTML = 'New Comment'
+
+    const paragraph = document.createElement('p')
+    paragraph.textContent = commentNew
+
+    listItem.appendChild(paragraph)
+    // console.log(listItem)
+
+    document.querySelector('ul.newComments').appendChild(listItem)
+    
+    commentInput.value = ''
+    }
+})
